8d4e1901eb484437a396aacf312d4c8ea931c19c
Amna Zeeshan <amnazee698@gmail.com>
2024-01-01 10:53:11 +0500

    Absolute Difference

--- src/LeetCode/AbsoluteDifference.java
+++ src/LeetCode/AbsoluteDifference.java
@@ -1,4 +1,23 @@
 package LeetCode;
 
 public class AbsoluteDifference {
+//    Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+public static void main(String[] arg){
+    int[][] sqMatrix={{1,2,3},{4,5,6},{9,8,9}};
+    int value=AbsoluteDiff(sqMatrix);
+    System.out.println("Absolute Difference is: "+value);
+}
+
+public static int AbsoluteDiff(int[][] matrix){
+    int sum1=0, sum2=0;
+    int absoluteDifference;
+    for(int i=0; i<matrix.length; i++){
+        sum1+=matrix[i][i];
+        sum2+=matrix[i][matrix.length-1-i];
+    }
+    System.out.println("Sum1: "+sum1);
+    System.out.println("Sum2: "+sum2);
+    absoluteDifference=sum1-sum2;
+    return absoluteDifference;
+}
 }
--- src/LeetCode/SecondMaxElement.java
+++ src/LeetCode/SecondMaxElement.java
@@ -1,4 +1,26 @@
 package LeetCode;
 
 public class SecondMaxElement {
+    public static void main(String[] arg){
+        int[] array={33,1,4,5,8,11,4,5,7,99};
+        System.out.println(maxAndSecondMax(array));
+    }
+
+    static int maxAndSecondMax(int[] array){
+        int max=0;
+        int secondMax=0;
+
+        for(int i=0; i<array.length;i++){
+            if(array[i]>max){
+                max=array[i];
+            }
+        }
+        for(int i=0; i<array.length; i++){
+            if(array[i]>secondMax && array[i]<max){
+                secondMax=array[i];
+            }
+        }
+
+        return secondMax;
+    }
 }
