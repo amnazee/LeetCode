62cd4659bc4f26b00339155858c8b34feccf1f0d
Amna Zeeshan <amnazee698@gmail.com>
2024-01-01 11:09:51 +0500

    Palindrome

--- src/LeetCode/PalindromeString.java
+++ src/LeetCode/PalindromeString.java
@@ -1,5 +1,18 @@
 package LeetCode;
 
 public class PalindromeString {
+    public static void main(String[] arg){
+        String palindrome="abba";
+        System.out.println(solution(palindrome));
+    }
+    static boolean solution(String inputString) {
+        int left = 0, right = inputString.length() - 1;
 
+        for (int i = 0; i < inputString.length() / 2; i++) {
+            if (inputString.charAt(left) != inputString.charAt(right)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
